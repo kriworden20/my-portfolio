06a45785dbec34923840d1f0afb83875bc6249df
Kristen Worden <krworden@umich.edu>
2020-09-12 23:38:42 -0400

    Updates

--- precache-manifest.410c86cf3c02bc30943df6fd45a8d480.js
+++ precache-manifest.410c86cf3c02bc30943df6fd45a8d480.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "8496b91f8388eff13061ef3b651d2dcb",
+    "revision": "cb6eeb5a2f22689b3493c0479b237324",
     "url": "/my-portfolio/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio/static/css/2.f6d03aaa.chunk.css"
   },
   {
-    "revision": "5fbb860d026c2ffd5f2c",
+    "revision": "5184b94086548fc9ce7b",
     "url": "/my-portfolio/static/css/main.f1043a89.chunk.css"
   },
   {
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/my-portfolio/static/js/2.04543333.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5fbb860d026c2ffd5f2c",
-    "url": "/my-portfolio/static/js/main.35b6169e.chunk.js"
+    "revision": "5184b94086548fc9ce7b",
+    "url": "/my-portfolio/static/js/main.4eed5611.chunk.js"
   },
   {
     "revision": "5c61d334295c7b140602",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-portfolio/precache-manifest.712d51bdc9a7b73882b37997b3bd25f0.js"
+  "/my-portfolio/precache-manifest.410c86cf3c02bc30943df6fd45a8d480.js"
 );
 
 self.addEventListener('message', (event) => {
