16cddca5234327865f41fd8e2ca601e8fb41ab7c
Kristen Worden <krworden@umich.edu>
2020-09-12 23:59:52 -0400

    Updates

---
+++ precache-manifest.5d03a1b63e13a1899f04946d14c3402a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "056b7042d21832504466ac405128a25e",
+    "url": "/my-portfolio/index.html"
+  },
+  {
+    "revision": "25af793f410b480b856c",
+    "url": "/my-portfolio/static/css/2.f6d03aaa.chunk.css"
+  },
+  {
+    "revision": "7fe52ae553f1a78de2b3",
+    "url": "/my-portfolio/static/css/main.f1043a89.chunk.css"
+  },
+  {
+    "revision": "25af793f410b480b856c",
+    "url": "/my-portfolio/static/js/2.72337f2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/my-portfolio/static/js/2.72337f2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7fe52ae553f1a78de2b3",
+    "url": "/my-portfolio/static/js/main.d65a1f1c.chunk.js"
+  },
+  {
+    "revision": "f241adf7bba56ceeeb82",
+    "url": "/my-portfolio/static/js/runtime-main.8b00eace.js"
+  }
+]);
